c22150882a387b7dbcb021621275c5b36d2660bf
gori-exe <emansaleem438@gmail.com>
2025-05-22 02:56:43 +0500

    fix navbar import case issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import myimg from './assets/hero.png'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
